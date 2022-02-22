350349dc3a4cdf13a7c5ed21e4e4428d94f0e97a
jimmy durak <deeppaz@hotmail.com>
2022-02-23 00:45:23 +0300

    new loading screen and func

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 import Logooo from "./assets/img/loading.png";
+import LoadingLogo from "./assets/img/loading.GIF";
 import Home from "./components/Home";
 import About from "./components/About";
 import Minting from "./components/Minting";
@@ -13,40 +14,39 @@ import ContactUs from "./components/ContactUs";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 5000);
+    window.addEventListener("load", handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
   }, []);
 
-  return (
-    <div>
-      {isLoading ? (
-        <div className="loading-div1">
-          <div className="loading-div2">
-            <div className="card-loading">
-              <div class="fold"></div>
-              <div class="fold"></div>
-              <div class="fold"></div>
-              <div class="fold"></div>
-              <div class="fold"></div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <Fragment>
-          <Header />
-          <div className="wrapper">
-            <BrowserRouter>
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/About" element={<About />} />
-                <Route exact path="/ContactUs" element={<ContactUs />} />
-                <Route exact path="/minting" element={<Minting />} />
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-          </div>
-        </Fragment>
-      )}
+  // useEffect(() => {
+  //   setTimeout(() => setIsLoading(true), 5000);
+  // }, []);
+
+  return !isLoading ? (
+    <Fragment>
+      <Header />
+      <div className="wrapper">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/About" element={<About />} />
+            <Route exact path="/ContactUs" element={<ContactUs />} />
+            <Route exact path="/minting" element={<Minting />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </Fragment>
+  ) : (
+    <div className="loading-div1">
+      <div className="loading-div2">
+        <img src={LoadingLogo} alt="loading logo" width={"10%"} />
+      </div>
     </div>
   );
 }
